1169801be5124777101de7a78adddf674f899468
tomarsunny2 <47889247+tomarsunny2@users.noreply.github.com>
2019-03-06 18:32:25 +0530

    removed source code
    Removed Source Code

--- products/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Product (
-    id   INTEGER      NOT NULL AUTO_INCREMENT ,
-    name VARCHAR(50) ,
-    price DECIMAL(10,2),
-    brand VARCHAR(50),
-    onsale Boolean,
-    PRIMARY KEY (id)
-);
--- products/src/test/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE product;
-CREATE TABLE Product (
-    id   INTEGER      NOT NULL AUTO_INCREMENT ,
-    name VARCHAR(50) ,
-    price DECIMAL(10,2),
-    brand VARCHAR(50),
-    onsale Boolean,
-    PRIMARY KEY (id)
-);
